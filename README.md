c063ee4f51a3a4b5428ab222ef6a0f064f61a0d7
Erik Dasque <erik@hungryfishmedia.com>
2013-01-30 17:56:33 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,55 @@
+AWS DynamoDBtoCSV
+==================
+
+This application will export the content of a DynamoDB table into CSV. All you need to do is create a *config.json* file in that same directory where you configure your *accessKeyId*, *secretAccessKey* and *region* as such:
+
+	{
+	    "accessKeyId": "REPLACE",
+	    "secretAccessKey": "REPLACE",
+	    "region": "REPLACE"
+	}
+
+Usage
+-------------------
+
+typically, to test you'd run:
+
+	node dynamoDBtoCSV.js -t Hourly_ZEDO_Impressions_by_IP
+
+to export to CSV
+
+and
+
+	node dynamoDBtoCSV.js -t Hourly_ZEDO_Impressions_by_IP -d
+
+to get some information about the table.
+
+Full syntax is:
+
+	node dynamoDBtoCSV.js --help
+		Usage: dynamoDBtoCSV.js [options]
+
+	Options:
+
+    	-h, --help               output usage information
+    	-V, --version            output the version number
+    	-t, --table [tablename]  Add the table you want to output to csv
+    	-d, --describe           
+
+
+Pre-requisites
+--------------
+You'll need to install a few modules, including:
+* dateformat
+* aws2js
+	
+	npm install aws-sdk commander
+
+should do it. 
+
+Example output
+--------------
+
+	"HashOf10","DateIPAdID","adcount"
+	"37693cfc748049e45d87b8c7d8b9aacd","2013011720024058205168000000010002","1"
+	"37693cfc748049e45d87b8c7d8b9aacd","2013011720050084232194000000010002","1"
---
+++ dynamoDBtoCSV.js
@@ -0,0 +1,92 @@
+var program = require('commander');
+var AWS = require('aws-sdk');
+AWS.config.loadFromPath('./config.json');
+var dynamoDB = new AWS.DynamoDB.Client();
+
+var iKnowTheHeaders = false;
+
+program.version('0.0.1').option('-t, --table [tablename]', 'Add the table you want to output to csv').option("-d, --describe").parse(process.argv);
+
+if(!program.table) {
+	console.log("You must specify a table");
+	program.outputHelp();
+	process.exit(1);
+}
+
+var query = {
+	"TableName": program.table,
+	"Limit": 1000,
+};
+
+
+var describeTable = function(query) {
+
+		dynamoDB.describeTable(query, function(err, data) {
+
+			if(!err) {
+
+				console.dir(data.Table);
+
+			} else console.dir(err);
+		});
+	}
+
+
+var scanDynamoDB = function(query) {
+
+		dynamoDB.scan(query, function(err, data) {
+
+			if(!err) {
+
+				printout(data.Items) // Print out the subset of results.
+				if(data.LastEvaluatedKey) { // Result is incomplete; there is more to come.
+					query.ExclusiveStartKey = data.LastEvaluatedKey;
+					scanDynamoDB(query);
+				};
+			} else console.dir(err);
+
+		});
+	};
+
+function arrayToCSV(array_input) {
+	var string_output = "";
+	for(var i = 0; i < array_input.length; i++) {
+		string_output += ('"' + array_input[i].replace('"', '\"') + '"')
+		if(i != array_input.length - 1) string_output += ","
+	};
+	return string_output;
+}
+
+function printout(items) {
+
+	if(!iKnowTheHeaders) {
+		var headers = [];
+		if(items.length > 0) {
+			for(var propertyName in items[0]) headers.push(propertyName)
+			console.log(arrayToCSV(headers))
+			iKnowTheHeaders = true;
+		}
+	}
+
+	for(index in items)
+
+	{
+
+		var values = []
+		for(var propertyName in items[index])
+
+		{
+			var value = (items[index][propertyName].N) ? items[index][propertyName].N : String(items[index][propertyName].S);
+			values.push(value)
+
+
+		}
+		console.log(arrayToCSV(values))
+
+
+
+	}
+
+}
+if(program.describe) describeTable(query);
+else scanDynamoDB(query);
