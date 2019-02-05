08e4e3a067f4b71f6e733759bd57e2cff4933371
Magno Gouveia <owlflee@gmail.com>
2019-02-05 19:33:02 -0200

    Implemented ES6 and ES7 features to improve code readability

--- dynamoDBtoCSV.js
+++ dynamoDBtoCSV.js
@@ -1,129 +1,140 @@
-var program = require('commander');
-var AWS = require('aws-sdk');
-var unmarshalItem = require('dynamodb-marshaler').unmarshalItem;
-var unmarshal = require('dynamodb-marshaler').unmarshal;
-var Papa = require('papaparse');
-var fs = require('fs');
-var headers = [];
-var unMarshalledArray = [];
+const program = require('commander');
+const AWS = require('aws-sdk');
+const unmarshal = require('dynamodb-marshaler').unmarshal;
+const Papa = require('papaparse');
+const fs = require('fs');
+const headers = [];
+const unMarshalledArray = [];
+const util = require('util');
+
+const fsWrite = util.promisify(fs.writeFile);
 
 program
-  .version('0.0.1')
-  .option('-t, --table [tablename]', 'Add the table you want to output to csv')
-  .option("-d, --describe")
-  .option("-r, --region [regionname]")
-  .option("-e, --endpoint [url]", 'Endpoint URL, can be used to dump from local DynamoDB')
-  .option("-p, --profile [profile]", 'Use profile from your credentials file')
-  .option("-f, --file [file]", "Name of the file to be created")
-  .parse(process.argv);
+	.version('0.0.1')
+	.option('-t, --table [tablename]', 'Add the table you want to output to csv')
+	.option("-d, --describe")
+	.option("-r, --region [regionname]")
+	.option("-e, --endpoint [url]", 'Endpoint URL, can be used to dump from local DynamoDB')
+	.option("-p, --profile [profile]", 'Use profile from your credentials file')
+	.option("-f, --file [file]", "Name of the file to be created")
+	.parse(process.argv);
 
-if (!program.table) {
-  console.log("You must specify a table");
-  program.outputHelp();
-  process.exit(1);
-}
-
-if (program.region && AWS.config.credentials) {
-  AWS.config.update({region: program.region});
-} else {
-  AWS.config.loadFromPath(__dirname + '/config.json');
-}
-
-if (program.endpoint) {
-  AWS.config.update({endpoint: program.endpoint})
-}
-
-if (program.profile) {
-  var newCreds = AWS.config.credentials;
-  newCreds.profile = program.profile;
-  AWS.config.update({credentials: newCreds});
-}
-
-var dynamoDB = new AWS.DynamoDB();
-
-var query = {
-  "TableName": program.table,
-  "Limit": 1000
-};
-
-var describeTable = function(query) {
-
-  dynamoDB.describeTable({
-    "TableName": program.table
-  }, function(err, data) {
-
-    if (!err) {
-
-      console.dir(data.Table);
-
-    } else console.dir(err);
-  });
-}
-
-
-var scanDynamoDB = function ( query ) {
-
-  dynamoDB.scan( query, function ( err, data ) {
-
-    if ( !err ) {
-      unMarshalIntoArray( data.Items ); // Print out the subset of results.
-      if ( data.LastEvaluatedKey ) { // Result is incomplete; there is more to come.
-        query.ExclusiveStartKey = data.LastEvaluatedKey;
-        scanDynamoDB(query);
-      }
-      else {
-		  let endData = Papa.unparse( { fields: [ ...headers ], data: unMarshalledArray } );
-		if(program.file){
-			writeData(endData)
-		}else{
-			console.log(endData);
-		}
-      }
-    }
-    else {
-      console.dir(err);
-    }
-  });
-};
-
-var writeData = function(data)
+if (!program.table)
 {
-	fs.writeFile(program.file, data, (err) => {
-		if(err) throw err;	
+	console.error("You must specify a table");
+	program.outputHelp();
+	process.exit(1);
+}
+
+if (program.region && AWS.config.credentials)
+{
+	AWS.config.update({region: program.region});
+} else
+{
+	AWS.config.loadFromPath(__dirname + '/config.json');
+}
+
+if (program.endpoint)
+{
+	AWS.config.update({endpoint: program.endpoint})
+}
+
+if (program.profile)
+{
+	const newCreds = AWS.config.credentials;
+	newCreds.profile = program.profile;
+	AWS.config.update({credentials: newCreds});
+}
+
+const dynamoDB = new AWS.DynamoDB();
+
+const query = {
+	"TableName": program.table,
+	"Limit": 1000
+};
+
+const describeTable = async function()
+{
+	try
+	{
+		let data = await dynamoDB.describeTable({
+			"TableName": program.table
+		}).promise();
+		console.dir(data.Table);
+	} catch (err)
+	{
+		console.dir(err);
+	}
+}
+
+const scanDynamoDB = async function(query)
+{
+	try
+	{
+		let data = await dynamoDB.scan(query).promise();
+		unMarshalIntoArray(data.Items); // Print out the subset of results.
+		if (data.LastEvaluatedKey)
+		{ // Result is incomplete; there is more to come.
+			query.ExclusiveStartKey = data.LastEvaluatedKey;
+			scanDynamoDB(query);
+			return;
+		}
+		let endData = Papa.unparse({fields: [...headers], data: unMarshalledArray});
+		if (program.file)
+		{
+			await writeData(endData)
+			return;
+		}
+		console.log(endData);
+	} catch (err)
+	{
+		console.dir(err);
+	}
+};
+
+const writeData = async function(data)
+{
+	try
+	{
+		await fsWrite(program.file, data);
 		console.log('File Saved');
+	} catch (err)
+	{
+		console.error(err);
+	}
+}
+
+const unMarshalIntoArray = function(items)
+{
+	if (items.length === 0)
+		return;
+
+	items.forEach(row =>
+	{
+		let newRow = {};
+		// console.log( 'Row: ' + JSON.stringify( row ));
+		Object.keys(row).forEach(function(key)
+		{
+			if (!headers.includes(key.trim()))
+			{
+				// console.log( 'putting new key ' + key.trim() + ' into headers ' + headers.toString());
+				headers.push(key.trim());
+			}
+			let newValue = unmarshal(row[key]);
+
+			if (typeof newValue === 'object')
+			{
+				newRow[key] = JSON.stringify(newValue);
+			}
+			else
+			{
+				newRow[key] = newValue;
+			}
+		});
+		// console.log( newRow );
+		unMarshalledArray.push(newRow);
 	});
 }
 
-function unMarshalIntoArray( items ) {
-  if ( items.length === 0 )
-    return;
-
-  items.forEach( function ( row ) {
-    let newRow = {};
-
-    // console.log( 'Row: ' + JSON.stringify( row ));
-    Object.keys( row ).forEach( function ( key ) {
-      if ( headers.indexOf( key.trim() ) === -1 ) {
-        // console.log( 'putting new key ' + key.trim() + ' into headers ' + headers.toString());
-        headers.push( key.trim() );
-      }
-      let newValue = unmarshal( row[key] );
-
-      if ( typeof newValue === 'object' ) {
-        newRow[key] = JSON.stringify( newValue );
-      }
-      else {
-        newRow[key] = newValue;
-      }
-    });
-
-    // console.log( newRow );
-    unMarshalledArray.push( newRow );
-
-  });
-
-}
-
-if ( program.describe ) describeTable( query );
-else scanDynamoDB( query );
-
+if (program.describe) describeTable();
+else scanDynamoDB(query);
